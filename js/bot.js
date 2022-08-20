249de0e1fb98d69562db74ff05d1cb3f67ec7756
Mikko Tervala <miksu103@gmail.com>
2022-08-20 20:22:04 +0300

    Add space between name and last name string

--- js/bot.js
+++ js/bot.js
@@ -111,7 +111,7 @@ var Bot = class {
             .then(({ filename, image }) => {
               var senderName = ctx.message.from.first_name
               if (config.showSenderLastName || (ctx.message.from.last_name !== undefined)) {
-                senderName = ctx.message.from.first_name + ctx.message.from.last_name
+                senderName = ctx.message.from.first_name + " " + ctx.message.from.last_name
               }
               var chatName = ''
               if (ctx.message.chat.type == 'group') {
